602babb807f7bc82717097c670f94c35e4ec04fd
Marina Barbosa Araujo <marina.ba@gmail.com>
2025-02-17 13:33:24 -0300

    aula 06

---
+++ aula06/Aula06.java
@@ -0,0 +1,13 @@
+package cursoemvideo.cursopoo.aula06;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        //c.ligarMudo();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
