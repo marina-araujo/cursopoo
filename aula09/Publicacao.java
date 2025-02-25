41be28cae83e913102220a3041a0278e802addd4
Marina Barbosa Araujo <marina.ba@gmail.com>
2025-02-25 10:20:48 -0300

    aula 09

---
+++ aula09/Publicacao.java
@@ -0,0 +1,9 @@
+package cursoemvideo.cursopoo.aula09;
+
+public interface Publicacao {
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void folhear(int p);
+    public abstract void avancarPagina();
+    public abstract void voltarPagina();
+}
